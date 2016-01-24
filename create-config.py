8fb8ed8c7e8fa1d34b851d3d47eb6b9c435679d2
Daniel Oaks <daniel@danieloaks.net>
2016-01-24 22:15:47 +1000

    create-config: Migrate to our new config format

--- create-config.py
+++ create-config.py
@@ -5,35 +5,74 @@ import getpass
 import json
 import sys
 
+from slugify import slugify
+
 if sys.version_info[0] < 3:
     in_func = raw_input
 else:
     in_func = input
 
-# using discord-irc/slack-irc format
-config = {}
+# custom format, kept in line with a 'format' key
+config = {
+    'format': 1,
+    'links': {},
+    'modules': {
+        'discord': {},
+        'irc': {},
+    },
+}
 
-config['nickname'] = in_func('IRC Nickname: ')
-config['server'] = in_func('IRC Server: ')
-config['discordEmail'] = in_func('Discord Email: ')
-config['discordPassword'] = getpass.getpass('Discord Password: ')
+config['modules']['discord']['email'] = in_func('Discord Email: ')
+config['modules']['discord']['password'] = getpass.getpass('Discord Password: ')
+config['modules']['irc']['nickname'] = in_func('IRC Nickname: ')
+config['modules']['irc']['server'] = in_func('IRC Server: ')
 
-chans = {}
+print('Link Configuration')
+print('To ignore a specific specific type of link, simply hit enter without specifying a name')
+links = {}
 while True:
-    print('Discord to IRC channel link - just hit enter to exit')
+    print()
+    name = in_func('Link Name: ').strip()
+    slug = slugify(name)
+    log = in_func('Log this link [n]? ').strip()
     discord_chan = in_func('Discord Channel: ').strip()
     irc_chan = in_func('IRC Channel: ').strip()
 
-    if not (discord_chan and irc_chan):
-        if len(chans) < 1:
-            print('You need to have at least one channel link')
-            continue
+    if slug in links:
+        overwrite = in_func('Link with that name already exists, overwrite existing link [y]? ').strip()
+        if len(overwrite) == 0 or overwrite[0].lower() in '1yt':
+            pass
         else:
-            break
+            print('Skipping link')
+            continue
 
-    chans[discord_chan] = irc_chan
+    if not (discord_chan and irc_chan):
+        print('You need to have one Discord channel and one IRC channel to link together')
+        continue
 
-config['channelMapping'] = chans
+    if len(log) >= 1 and log[0].lower() in '1yt':
+        log = True
+        print('Logging link')
+    else:
+        log = False
+        print('Will not log link')
+
+    links[slug] = {
+        'name': name,
+        'log': log,
+        'channels': {
+            'discord': discord_chan,
+            'irc': irc_chan,
+        },
+    }
+
+    another_link = in_func('Setup another link [n]? ')
+    if len(another_link) and another_link[0].lower() in '1yt':
+        pass
+    else:
+        break
+
+config['links'] = links
 
 with open('config.json', 'w') as f:
     json.dump([config], f, indent=2, sort_keys=True)
